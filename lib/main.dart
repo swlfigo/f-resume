22da19d17b38a793796eaacc3b6c6ac7a52cb2ff
Sylar <swlfigo@gmail.com>
2024-12-06 16:18:58 +0800

    Add Welcom Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fresume/resume/resume_main.dart';
+import 'package:fresume/welcome/welcome_splash.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const GetMaterialApp(
     title: 'My Resume',
-    home: ResumeMain(),
+    home: WelcomeSplash(),
   ));
 }
